39c96ae9d029ec0c3c6aaf8fddbeb9a9c0e05b16
Manuel <manuel@boccaperta.com>
2015-10-23 11:21:13 +0200

    Implement filter with reduce

--- src/clj_brave_and_true/chapter4.clj
+++ src/clj_brave_and_true/chapter4.clj
@@ -9,3 +9,15 @@
           s))
 
 (my-map inc [1 2 3])
+
+;;; implement filter with reduce
+(defn my-filter
+  [p s]
+  (reduce (fn [new-s s]
+            (if (p s)
+              (cons s new-s)
+              new-s))
+          []
+          s))
+
+(my-filter #(> % 2) [1 2 3 4 5])
